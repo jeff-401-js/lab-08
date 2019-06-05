864e3911d740b1d62c21a529a792db64e6495f35
JeffLawrence1 <jefflaw13@hotmail.com>
2019-06-05 15:07:52 -0700

    category and product work

--- src/models/categories.js
+++ src/models/categories.js
@@ -1,11 +1,15 @@
 'use strict';
 
+const schema = require('./categories-schema.js');
+
 class Categories {
 
   constructor() {
   }
 
   get(_id) {
+    let queryObject = _id ? {_id} : {};
+    return schema.find(queryObject);
   }
   
   post(record) {
